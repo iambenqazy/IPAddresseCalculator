52c38effe76a58848d52ad4ddb26aee753372798
Kwame Benqazy <iambenqazy@gmail.com>
2020-09-10 22:13:11 +0000

    modified all functions

--- ipaddressecalc.py
+++ ipaddressecalc.py
@@ -5,9 +5,10 @@ from typing import List, Tuple
 
 class IPOctet:
 
-    def __init__(self, ip_address, subnet_mask):
+    def __init__(self, ip_address, subnet_mask, networks_needed):
         self.ip_address = ip_address
         self.subnet_mask = subnet_mask
+        self.networks_needed = networks_needed
         self.OCTET_MAX_VALUE = 255
         self.OCTET_MIN_VALUE = 0
         self.TOTAL_SUBNET_BIT = 32
@@ -21,10 +22,22 @@ class IPOctet:
         """
         return self.OCTET_MIN_VALUE <= int(number) <= self.OCTET_MAX_VALUE
 
+    def check_octet_position(self, subnet_mask=None):
+        """
+        This checks the subnet mask bit and returns the octet position
+        :param subnet_mask:
+        :return: int
+        """
+        subnet_bit = subnet_mask or self.subnetmask_decimal_to_cidr()
+        if subnet_bit != 32:
+            return int(subnet_bit / 8) + 1
+        else:
+            return int(subnet_bit / 8)
+
     def split_address(self, input_address) -> List[int]:
         """
-        Validate octet. an octet should be numbers separated by dots
-        :return a list and a boolean
+        This splits a string of input address into a list and checks if its numeric
+        :return a list
         """
         address_value = input_address.split('.')
         for address in address_value:
@@ -43,33 +56,94 @@ class IPOctet:
         """
         return self.split_address(self.ip_address)
 
-    def validate_subnetmask(self):
+    def validate_subnetmask(self, input_subnetmask=None):
         """
         This returns the validated form of the input subnetmask number
         :return: int if cidr notation(/24 -> 24) or list if decimal notation(255.255.255.0 -> [255],[255],[255],[0])
         """
-        input_subnetmask = self.subnet_mask
-        input_subnetmask_length = len(input_subnetmask)
+        input_subnetmask_value = input_subnetmask or self.subnet_mask  # this assigns the subnet_mask if the input
+        # parameter is None
+        input_subnetmask_length = len(input_subnetmask_value)
 
-        if (input_subnetmask_length == 3) and (input_subnetmask.startswith('/')):
-            if (input_subnetmask[1:].isnumeric()) and (int(input_subnetmask[1:]) <= self.TOTAL_SUBNET_BIT):
-                cidr_subnetmask = int(input_subnetmask[1:])
+        if (input_subnetmask_length == 3) and (input_subnetmask_value.startswith('/')):
+            input_cidr_bit = input_subnetmask_value.split('/')
+            if (input_cidr_bit[1].isnumeric()) and (int(input_cidr_bit[1]) <= self.TOTAL_SUBNET_BIT):
+                cidr_subnetmask = int(input_cidr_bit[1])
                 return cidr_subnetmask
         elif 6 < input_subnetmask_length < 16:
-            return self.split_address(input_subnetmask)  # validate and return a list of the address
+            new_input_subnetmask = self.split_address(input_subnetmask_value)
+            for octet_index, octet in enumerate(new_input_subnetmask):
+                for octet_position in range(0, octet_index):
+                    if octet <= new_input_subnetmask[octet_position]:
+                        continue
+                    else:
+                        raise ValueError("Invalid Subnet Value")
+            return new_input_subnetmask  # validate and return a list of the address
         else:
-            raise ValueError("Invalid CIDR Number.")
+            raise ValueError("Invalid Subnet Number")
 
-    def network_bits_needed(self, number=5):
+    def subnetmask_cidr_to_decimal(self, subnet_value=None):
+        """
+        This convert subnet bit value from cidr to decimal notation
+        :return: list
+        """
+        if subnet_value:
+            subnet_value = self.validate_subnetmask(subnet_value)
+        subnet_bit = subnet_value or self.validate_subnetmask()
+        bit_value = self.bintodec_list
+        subnet_list = []
+
+        if type(subnet_bit) is int:
+            if not 8 <= subnet_bit <= 32:
+                raise ValueError("Invalid Subnet Address")
+            for octet in range(0, 4):
+                if not subnet_bit < 8:
+                    subnet_list.insert(octet, sum(bit_value))
+                    subnet_bit -= 8
+                elif not subnet_bit == 0:
+                    subnet_list.insert(octet, sum(bit_value[:subnet_bit]))
+                    subnet_bit -= subnet_bit
+                else:
+                    subnet_list.insert(octet, subnet_bit)
+
+            if len(subnet_list) == 4:
+                return subnet_list
+            else:
+                return []
+
+    def subnetmask_decimal_to_cidr(self, subnet_mask=None):
+        """
+        This converts decimal notation to cidr of the subnetmask
+        :param : list
+        :return: int for the bit value
+        """
+        subnet_id = subnet_mask or self.validate_subnetmask()
+        cidr_bit = 0
+        if type(subnet_id) is list:
+            for address in subnet_id:
+                cidr_bit += bin(address).count('1')
+            return cidr_bit
+        else:
+            return subnet_id
+
+    def network_bits_needed(self, input_network_value=None):
         """
         Steps Based on Networks needed
         1. Convert the number of networks that you need to binary <NB: It’s always bes®t to subtract one from the
         number of networks needed before calculating the number of bits: eg. 5 networks; 5 - 1 = 4>
+        :param: int
+        :return: int
         """
-        if number <= 1:
+        network_number = input_network_value or self.networks_needed
+        if network_number.isnumeric():
+            network_number = int(network_number)
+        else:
+            raise ValueError("Invalid Address Number.")
+
+        if network_number <= 1:
             return 0
-        input_bit = len(bin(number - 1)[2:])
-        remaining_bit = self.TOTAL_SUBNET_BIT - self.validate_subnetmask()
+        input_bit = len(bin(network_number - 1)[2:])
+        remaining_bit = self.TOTAL_SUBNET_BIT - self.subnetmask_decimal_to_cidr()
         host_bit = remaining_bit - input_bit
         if host_bit <= 1:
             raise ValueError("Invalid Network Number")
@@ -85,6 +159,11 @@ class IPOctet:
         if subnet_number > 30 or subnet_number < 0:
             raise ValueError("Invalid Subnet Number")
         return subnet_number
+
+    def number_of_hosts_per_network(self):
+        host_bit = self.TOTAL_SUBNET_BIT - self.subnet_number_needed()
+        host_number = (2 ** host_bit) - 2
+        return host_number
 
     def ip_class_name(self):
         """
@@ -104,63 +183,35 @@ class IPOctet:
         elif address in octet_range[240:]:
             return "Class E"
 
-    def subnetmask_cidr_conversion(self, subnet_bit):
-        """
-        This convert subnetmask id from cidr to decimal notation and vice versa
-        :return:
-        """
-        bit_value = self.bintodec_list
-        subnet_list = []
-        if not 8 <= subnet_bit <= 32:
-            raise ValueError("Invalid Subnet Address")
-        for octet in range(0, 4):
-            if not subnet_bit < 8:
-                subnet_list.insert(octet, sum(bit_value))
-                subnet_bit -= 8
-            elif not subnet_bit == 0:
-                subnet_list.insert(octet, sum(bit_value[:subnet_bit]))
-                subnet_bit -= subnet_bit
-            else:
-                subnet_list.insert(octet, subnet_bit)
-
-        if len(subnet_list) == 4:
-            return subnet_list
-        else:
-            return []
-
     def network_increment_number(self):
-        increment_position = (self.validate_subnetmask() + self.network_bits_needed()) % 8
+        increment_position = (self.subnetmask_decimal_to_cidr() + self.network_bits_needed()) % 8
         increment_number = self.bintodec_list[increment_position - 1]
         return increment_number
 
-    def network_id(self, if_range=True):
+    def network_id(self):
         """
         Calculate and return a list of all network ids
         :return: a list
         """
-        if self.network_bits_needed() == 0:
-            return 0
+        network_bits_needed = self.network_bits_needed()
+        if network_bits_needed == 0:
+            return [0]
 
-        range_list = [0]
         range_increment = self.network_increment_number()
-        networks_needed = 2 ** self.network_bits_needed()
+        number_of_networks = 2 ** network_bits_needed
 
-        for ip_number in range(0, networks_needed):
-            range_list.append(range_list[ip_number] + range_increment)
-
-        if if_range:
-            return range_list
-        else:
-            return networks_needed
+        range_list = list(range(0, (2**number_of_networks), range_increment))
+        return range_list
 
     def broadcast_id(self):
         """
         Return the last number in the ip address list
-        :return: integer
+        :return: list
         """
         if self.network_bits_needed() == 0:
-            return 255
+            return [255]
         network_id_range = self.network_id()[1:]
+        network_id_range.append(256)
         broadcast_id_range = [ip_number - 1 for ip_number in network_id_range]
 
         return broadcast_id_range
@@ -170,14 +221,8 @@ class IPOctet:
         Calculates the valid host range per network
         :return: list
         """
-        # host_bit = self.TOTAL_SUBNET_BIT - (self.subnet_mask + self.network_bits_needed())
-        # host_number = (2 ** host_bit) - 2
-
         network_ips = self.network_map_ip()
-
-        host_ip = []
-        for network in range(1, len(network_ips) + 1):
-            host_ip.append(list(network_ips.get(network, ())))  # this returns a list of values in the dictionary
+        host_ip = [list(network_ips.get(network, ())) for network in range(1, len(network_ips) + 1)]
 
         for host_list in host_ip:
             for number in range(len(host_list)):
@@ -186,7 +231,8 @@ class IPOctet:
                 elif number == 1:
                     host_list[number] -= 1
 
-        return host_ip
+        host_ip_addresse = dict(enumerate(host_ip, 1))
+        return host_ip_addresse
 
     def join_ip_address(self):
         """
@@ -205,34 +251,36 @@ class IPOctet:
         """
         if self.network_bits_needed() == 0:
             return {1: (0, 255)}
-        network = self.network_id()[:-1]
-        broadcast = self.broadcast_id()
-        network_number = list(range(1, self.network_id(False) + 1))
-        network_broadcast = zip(network, broadcast)
-        network_ip_number = zip(network_number, network_broadcast)
 
-        return dict(network_ip_number)
+        network = self.network_id()
+        broadcast = self.broadcast_id()
+        network_broadcast = zip(network, broadcast)
+
+        return dict(enumerate(network_broadcast, 1))
 
 
 if __name__ == '__main__':
-    ip = IPOctet('192.168.8.8', '/22')
+    ip = IPOctet('192.168.8.8', '/24', '5')
     host_ip_range = ip.host_id()
     subnet_bit_value = ip.subnet_number_needed()
-    len_of_host = len(range(host_ip_range[0][0], host_ip_range[0][1])) + 1
-    if isinstance(ip.network_id(), int) and isinstance(ip.broadcast_id(), int):
-        network_range = [ip.network_id()]
-        broadcast_range = [ip.broadcast_id()]
-    else:
-        network_range = ip.network_id()[:-1]
-        broadcast_range = ip.broadcast_id()
+    total_hosts = ip.number_of_hosts_per_network()
+    network_range = ip.network_id()
+    broadcast_range = ip.broadcast_id()
+    # if isinstance(ip.network_id(), int) and isinstance(ip.broadcast_id(), int):
+    #     network_range = [ip.network_id()]
+    #     broadcast_range = [ip.broadcast_id()]
+    # else:
+    #     network_range = ip.network_id()
+    #     broadcast_range = ip.broadcast_id()
     len_of_network = len(network_range)
-    network_map = list(zip(network_range, broadcast_range))
+    network_map = dict(enumerate(zip(network_range, broadcast_range), 1))
     print(f"Host IP range: {host_ip_range}")
     print(f"Network and Broadcast ID range: {network_map}")
 
-    print(f"This is a {ip.ip_class_name()} address and there are {len_of_network} networks with {len_of_host} "
+    print(f"This is a {ip.ip_class_name()} address and there are {len_of_network} networks with {total_hosts} "
           f"valid host ip addresses per network and a subnet mask of /{subnet_bit_value}"
-          f" or {'.'.join(map(str, ip.subnetmask_cidr_conversion(subnet_bit_value)))}")
+          f" or {'.'.join(map(str, ip.subnetmask_cidr_to_decimal(subnet_bit_value)))}")
+    print(f"The Input subnet is {'.'.join(map(str, ip.subnetmask_cidr_to_decimal(ip.validate_subnetmask())))}")
 
     # for index in range(0, len(ip.network_id())):
     #     print(f"Network {index + 1}: ")
